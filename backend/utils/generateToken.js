9b266b888b77796958b389343ed40038d731b161
Kabilesh <kabilesh@dal.ca>
2024-07-29 19:06:58 -0300

    cookir

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,15 +4,15 @@ const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: '30d',
   });
-console.log(token);
 
   // Set JWT as an HTTP-Only cookie
   res.cookie('jwt', token, {
     httpOnly: true,
     sameSite: 'strict', // Prevent CSRF attacks
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-    domain: 'http://54.156.109.164/',
+    domain: '54.156.109.164', // Only domain or IP, without protocol
     secure: process.env.NODE_ENV === 'production',
+    path: '/', // Ensure the cookie is available for all routes
   });
 };
 
