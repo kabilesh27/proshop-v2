dc0e1a2725d6cfbb4f8de99001bb126cb14aa28b
Kabilesh <kabilesh@dal.ca>
2024-07-29 18:42:24 -0300

    cookies

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,7 +9,6 @@ console.log(token);
   // Set JWT as an HTTP-Only cookie
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
     sameSite: 'strict', // Prevent CSRF attacks
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
