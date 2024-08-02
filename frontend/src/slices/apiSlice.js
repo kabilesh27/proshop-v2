a6eeb3eccc5cc165fb088241eafcc5981595a264
Kabilesh <kb320903@dal.ca>
2024-08-02 15:33:41 -0300

    change

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -9,7 +9,7 @@ import { logout } from './authSlice'; // Import the logout action
 // https://redux-toolkit.js.org/rtk-query/usage/customizing-queries#customizing-queries-with-basequery
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: BASE_URL,
+  baseUrl: "http://3.92.18.142:5000",
 });
 
 async function baseQueryWithAuth(args, api, extra) {
